a8dba3706f913fc306daacd897750635d42c99c0
Jacek Rudziński <jacek.Rudzinski@skanska.pl>
2016-05-16 10:17:42 +0200

    Poprawki aliasów

--- _aliases-init.bash
+++ _aliases-init.bash
@@ -4,6 +4,12 @@ echo 'Add g as git alias'
 alias g='git'
 complete -o default -o nospace -F _git g
 
+if [[ -e /c/Program\ Files\ \(x86\)/Notepad++/notepad++.exe ]]
+then	
+	alias notepad2='/c/Program\ Files\ \(x86\)/Notepad++/notepad++.exe -multiInst -notabbar -nosession -noPlugin'
+	echo 'Notepad++ alias registered'
+fi
+
 echo 'Include aliases from aliases.gitconfig'
 aliasesFilePath="$(dirname "${BASH_SOURCE[0]}")/aliases.gitconfig"
 git config --global include.path $aliasesFilePath

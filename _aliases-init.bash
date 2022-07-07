d3768653f83c5a2aae3fb383bf2f0e9a600f4ad9
Jacek Rudziński <jadz@demant.com>
2022-07-07 13:45:59 +0200

    Added new aliases.

--- _aliases-init.bash
+++ _aliases-init.bash
@@ -4,7 +4,7 @@ LC_CTYPE=en_US.utf8
 
 echo 'Add g as git alias'
 alias g='git'
-complete -o default -o nospace -F _git g
+complete -o default -o nospace -F __git_wrap__git_main g
 
 echo 'Add notepad alias'
 if [[ -e /c/Program\ Files\ \(x86\)/Notepad++/notepad++.exe ]]
@@ -17,7 +17,7 @@ aliasesFilePath="$(dirname "${BASH_SOURCE[0]}")/aliases.gitconfig"
 git config --global include.path $aliasesFilePath
 
 echo 'Register aliases autocompletions functions'
-_git_not_merged_list () 
+_git_not_merged_list() 
 {
     _git_branch
 }
